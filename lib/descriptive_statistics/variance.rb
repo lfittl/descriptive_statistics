4e76e0ff72a866d1efe04f6c96f11199a2f5cc4a
Lukas Fittl <lukas@fittl.com>
2013-05-29 15:05:18 +0200

    Make variance work on empty arrays.

--- lib/descriptive_statistics/variance.rb
+++ lib/descriptive_statistics/variance.rb
@@ -1,6 +1,7 @@
 module DescriptiveStatistics
   def variance
     mean = self.mean
-    self.map{ |sample| (mean - sample) ** 2 }.inject(:+) / self.number
+    return 0.0 unless sum = self.map{ |sample| (mean - sample) ** 2 }.inject(:+)
+    sum / self.number
   end
 end
